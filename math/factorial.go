02f694200114898fe88761bf9d3f6b5b949ca7c7
Salavat Yalalov <sialalov@exadel.com>
2020-07-16 23:17:22 +0500

    calculate factorial with using goroutines and channels

--- math/factorial.go
+++ math/factorial.go
@@ -1,9 +1,15 @@
 package math
 
-import "math/big"
+import (
+	"fmt"
+	"log"
+	"math/big"
+)
 
 // FactorialTree Calculate factorial by tree calculation method
 func FactorialTree(n int) *big.Int {
+	threads := 2
+
 	if n < 0 {
 		return big.NewInt(0)
 	}
@@ -16,7 +22,42 @@ func FactorialTree(n int) *big.Int {
 		return big.NewInt(int64(n))
 	}
 
-	return prodTree(2, n)
+	if n < threads+1 {
+		return prodTree(2, n)
+	}
+
+	// TODO replace the code below which works only for even N
+	ch := make(chan *big.Int)
+	upper := n / threads
+	log.Printf("Before first goroutine left = %d, right = %d", 2, upper)
+	go goProdTree(2, upper, ch)
+
+	for i := 1; i < threads; i++ {
+		left := (n/threads)*i + 1
+		right := (n / threads) * (i + 1)
+		log.Printf("Before goroutine left = %d, right = %d, i = %d", left, right, i)
+		go goProdTree(left, right, ch)
+	}
+
+	// TODO the code below had to return the calculated factorial
+	// need to read the golang doc about processing the messages when multiple senders produce
+	// the messages into one channel
+	f := big.NewInt(1)
+	for {
+		select {
+		case v := <-ch:
+			fmt.Printf("v = %s\n", v.String())
+			f.Mul(f, v)
+		}
+	}
+
+	// return f
+}
+
+func goProdTree(left, right int, ch chan *big.Int) {
+	res := prodTree(left, right)
+	log.Printf("res = %s\n", res.String())
+	ch <- res
 }
 
 func prodTree(left, right int) *big.Int {
